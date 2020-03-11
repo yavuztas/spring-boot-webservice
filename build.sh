b3253ce9dee9ed8c2034726e3619b787b5519e77
Yavuz Tas <yavuzslmtas@gmail.com>
2020-03-11 21:25:21 +0100

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 mvn clean install
+docker container rm spring-boot-webservice
 docker build --build-arg USER=webservice -t boilerplates/spring-boot-webservice-0.0.1 .
 docker run -p 8080:8000 --name spring-boot-webservice -e JAVA_OPTS=-Dserver.port=8000 boilerplates/spring-boot-webservice-0.0.1
